f32fa6d5b900524ce22135e5ba2cbc388ab125fd
Aluno <Aluno@fapi.local>
2019-03-07 20:27:12 -0300

    Um pequeno salto para o Homem, mas um grande salto para humanidade

---
+++ src/Ex02.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner Ler = new Scanner(System.in);
+	
+	int n1, n2;
+	System.out.println("Entre com o primeiro inteiro: ");
+    n1 = Ler.nextInt();
+    System.out.println("Entre com o segundo inteiro: ");
+    n2 = Ler.nextInt();
+				
+    if(n1 < n2) {
+    	System.out.println("O primeiro valor e menor: " +n1);
+    }else { 
+    	if (n1 == n2){
+    		System.out.println("Ambos tem o mesmo valor" );
+    }else {
+    	System.out.println("O segundo valor e menor: " +n2);
+    }
+	}
+	}
+}
